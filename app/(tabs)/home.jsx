619d938ae0fd91d007858a6206399cfb1364bb34
Niwandi <niwandihewagama@gmail.com>
2024-11-06 19:04:16 +0530

    initial commit

---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?
+            </Text>
+           
+          </View>
+  )
+}
+
+export default Home
